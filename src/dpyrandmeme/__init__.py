e08953b903e811672d2a2a97fe87fcba1f847deb
Defy <61808223+De-y@users.noreply.github.com>
2023-03-04 17:04:00 -0800

    1.0.0!
    Version 1

--- src/dpyrandmeme/__init__.py
+++ src/dpyrandmeme/__init__.py
@@ -3,7 +3,12 @@
 #   Licensed under the MIT License. See LICENSE in project root for information.
 #   -------------------------------------------------------------
 """
-File for fetching and setting the Python Random Meme as an embed.
+dpyrandmeme: discord.py-random-meme
+
+Random memes to be used in discord.py
+
+Copyright (c) aviance. All rights reserved.
+Licensed under the MIT License. See LICENSE in project root for information.
 """
 
 from __future__ import annotations
@@ -11,11 +16,17 @@ import aiohttp
 import discord
 import random
 
-__version__ = "0.0.3"
+__version__ = "1.0.0"
 
 
 
-async def pyrandmeme():
+async def pyrandmeme(hex_color):
+    """
+        Get Random Meme from a random subreddit and return it inside of an discord embed.
+        Usage: pyrandmeme(hex_color)
+        Remember to strip the # at the start, or it won't work.
+    """
+    color = '0x' + hex_color
     lists_for_memes = ['https://www.reddit.com/r/GoCommitDie/rising.json?sort=rising',
                        'https://www.reddit.com/r/okbuddyretard/top.json?sort=top', 
                        'https://www.reddit.com/r/memes/rising.json?sort=rising', 
@@ -39,8 +50,8 @@ async def pyrandmeme():
         async with cs.get(lists_for_memes[random.randint(0, 16)]) as r:
             res = await r.json()
             var=res['data']['children'][random.randint(0, 16)]['data']
-            pymeme = discord.Embed(url="https://www.reddit.com" + var['permalink'], title="**" + var['title'] + "**", color=0xe91e63)
+            pymeme = discord.Embed(url="https://www.reddit.com" + var['permalink'], title="**" + var['title'] + "**", color=color)
             pymeme.set_image(url=var['url'])
             pymeme.set_footer(text='Post made by u/' + var['author'] + ' and posted in subreddit r/' + var['subreddit'] + '.')
             return pymeme
-        await pyrandmeme()
+        await pyrandmeme('e91e63')
